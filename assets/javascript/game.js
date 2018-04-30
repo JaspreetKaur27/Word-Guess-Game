ad44102260702aa5a86dd0f3c4ffce197aecc928
Jaspreet Kaur <jaspreetr86@gmail.com>
2018-04-29 22:12:54 -0400

    add alerts and some peseudocode for image displaying with amtching word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,15 +5,18 @@ var $guessedletters = document.getElementById('guessed-letters');
 var $guessesleft = document.getElementById('guesses-left');
 var $wins = document.getElementById('wins');
 var $losses = document.getElementById('losses');
-var $message = document.getElementById('message');
-var $msgtitle = document.getElementById('msgtitle');
-var $msgtext = document.getElementById('msgtext');
-var $image = document.getElementById('image');
+// var $message = document.getElementById('message');
+// var $msgtitle = document.getElementById('msgtitle');
+// var $msgtext = document.getElementById('msgtext');
+// var $image = document.getElementById('image');
+
 
 
 //Created local variables to do JAvascript Functionality
-var wordlist= ['apple','bananas','grapes'];
-var images = ['assets/images/apple.jpg','assets/images/bananas.jpg', 'assets/images/grapes.jpg']
+var wordlist= ['apple','bananas','grapes','peach','pineapple','strawberry', 'blackberry', 'blueberry','plump'];
+
+
+
 var wins = 0;
 var losses = 0;
 var guessesleft = 9;
@@ -96,8 +99,9 @@ function checkLose()
         {   
             losses++;          
             $losses.textContent = losses;
-            alert("Oops you lose. The word was: " + pickedword);
             newgame();
+            alert("Oops you lose. The word was: " + pickedword + ". Let's try another word.");
+            
         }           
         checkWin();           
 }
@@ -109,6 +113,7 @@ function checkWin()
         wins++;  
         $wins.textContent = wins; 
         newgame();   
+        alert("Awesome, you win. Now try next word!")
     }
 }
 
@@ -127,3 +132,21 @@ document.onkeypress = function(event)
         newgame();
     }
 }
+
+
+//Pseudocode for image displaying
+
+//for displaying image matching to the word we can user key, value pair in the array and can loop through when we need to 
+//display text according to user guesses or image of matching word as value of key name 
+// we can make array as like follows
+// var wordlist =[
+//     {name: 'apple', image: 'assets/images/apple.jpg'},
+//     {name: 'bananas', image: 'assets/images/bananas.jpg'},
+//     {name: 'grapes', image: 'assets/images/grapes.jpg'},
+//     {name: 'orange', image: 'assets/images/orange.jpg'},
+//     {name: 'peach', image: 'assets/images/peach.jpg'},
+//     {name: 'plump', image: 'assets/images/plump.jpg'},
+//     {name: 'raspberry', image: 'assets/images/raspberry.jpg'},
+//     {name: 'strawberry', image: 'assets/images/strawberry.jpg'},
+//     {name: 'blackberry', image: 'assets/images/blackberries.jpg'},   
+// ]
